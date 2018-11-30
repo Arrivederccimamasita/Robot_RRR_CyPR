5cd5cc3d1a9b361c099f76bcb7874208a4846a57
marrcogrova <marrcogrova@gmail.com>
2018-11-30 19:38:50 +0100

    luchando contra el robot real sin

--- ModeloDinamico_RobotReal_sR.m
+++ ModeloDinamico_RobotReal_sR.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% FUNCIÓN EMPLEADA PARA EJECUTAR LOS DATOS OBTENIDOS A TRAVÉS DEL SCRIPT  %
+% FUNCIï¿½N EMPLEADA PARA EJECUTAR LOS DATOS OBTENIDOS A TRAVï¿½S DEL SCRIPT  %
 % "DinamicaRobotNE.m" EN SIMULINK MEDIANTE DEL ARCHIVO                    %
 % "sl_R3GDL_Mfunction.mdl"                                                %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -20,24 +20,23 @@ g=9.8;L0=0.6;L1=0.6;L2=1;L3=0.8;
 R1=1; R2=1; R3=1;
 
 % Matriz de Inercias
-M=[ 5.7477*cos(2.0*q2) - 0.0074824*cos(2.0*q2 + q3) - 0.0074824*cos(q3) + 2.1857*cos(2.0*q2 + 2.0*q3) - 3.0872,                             0,                              0;
-    0,       15.3 - 0.018706*cos(q3), - 0.0093529*cos(q3) - 0.014965;
-    0, - 0.010689*cos(q3) - 0.017103,                          66.87];
+M=[ 4.434*cos(2.0*q2 + q3) + 5.7477*cos(2.0*q2) + 4.434*cos(q3) + 5.7389*cos(2.0*q2 + 2.0*q3) + 0.46597,                       0,                       0;
+                                                                                                   0,  11.085*cos(q3) + 21.67, 5.5425*cos(q3) + 6.3549;
+                                                                                                   0, 6.3343*cos(q3) + 7.2628,                    7.56];
+ 
 
-
-% Matriz de aceleraciones centrípetas y de Coriolis
-
-V=[1.7347e-19*qd1*(8.6266e16*qd2*sin(2.0*q2 + q3) + 4.3133e16*qd3*sin(2.0*q2 + q3) - 6.6267e19*qd2*sin(2.0*q2) + 4.3133e16*qd3*sin(q3) - 2.5199e19*qd2*sin(2.0*q2 + 2.0*q3) - 2.5199e19*qd3*sin(2.0*q2 + 2.0*q3) + 5.2383e20);
-    136.82*qd2 + 0.0093529*qd3^2*sin(q3) + 2.7321*qd1^2*sin(2.0*q2 + 2.0*q3) - 0.0093529*qd1^2*sin(2.0*q2 + q3) + 7.1847*qd1^2*sin(2.0*q2) + 0.018706*qd2*qd3*sin(q3);
-    4.1699*qd3 - 0.0053445*qd1^2*sin(q3) - 0.010689*qd2^2*sin(q3) + 3.1224*qd1^2*sin(2.0*q2 + 2.0*q3) - 0.0053445*qd1^2*sin(2.0*q2 + q3)];
-
-% Par gravitatorio
-G=[                                               0;
-      -3.2526e-18*g*(2.8755e15*cos(q2 + q3) - 5.1888e18*cos(q2));
-      -0.010689*g*cos(q2 + q3)];
+% Matriz de aceleraciones centrï¿½petas y de Coriolis
+V=[ -1.3878e-18*qd1*(6.3901e18*qd2*sin(2.0*q2 + q3) + 3.195e18*qd3*sin(2.0*q2 + q3) + 8.2833e18*qd2*sin(2.0*q2) + 3.195e18*qd3*sin(q3) + 8.2706e18*qd2*sin(2.0*q2 + 2.0*q3) + 8.2706e18*qd3*sin(2.0*q2 + 2.0*q3) - 2.6192e16);
+                                                                0.15202*qd2 - 5.5425*qd3^2*sin(q3) + 7.1736*qd1^2*sin(2.0*q2 + 2.0*q3) + 5.5425*qd1^2*sin(2.0*q2 + q3) + 7.1847*qd1^2*sin(2.0*q2) - 11.085*qd2*qd3*sin(q3);
+                                                                                            0.018533*qd3 + 3.1671*qd1^2*sin(q3) + 6.3343*qd2^2*sin(q3) + 8.1984*qd1^2*sin(2.0*q2 + 2.0*q3) + 3.1671*qd1^2*sin(2.0*q2 + q3)];
+ 
+% Par gravitatorio                                                          
+G=[                                           0;
+ 2.5*g*(2.217*cos(q2 + q3) + 6.7509*cos(q2));
+                       6.3343*g*cos(q2 + q3)];
 %M=vpa(M,5); V=vpa(V,5); G=vpa(G,5);
 
-% Ecuación del robot
+% Ecuaciï¿½n del robot
 %    Im = M*qpp + V + G
 Im=[Im1;Im2;Im3];
 
