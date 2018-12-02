248484fd50378b50df32e6e0ad4c212f999d6953
marrcogrova <marrcogrova@gmail.com>
2018-12-02 16:48:04 +0100

    again

--- Declaraciones.c
+++ Declaraciones.c
@@ -1,16 +1,16 @@
-/* DECLARACIÓN DE VARIABLES */
+/* DECLARACIï¿½N DE VARIABLES */
 
 /*
  * Ya definidas:
- *   - Ureal:    señal de control (salida el controlador)
+ *   - Ureal:    seï¿½al de control (salida el controlador)
  *   - Yk:       medida de la salida del sistema (entrada del controlador)
- *   - POutAux : salida auxiliar de libre disposición
+ *   - POutAux : salida auxiliar de libre disposiciï¿½n
  *   - Tm:       tiempo de muestreo
  */
 
-  double tiempo; /* Para tiempo de simulación */
+  double tiempo; /* Para tiempo de simulaciï¿½n */
   static double ek1[3]; /* Valores anteriores del error */
-  static double uk1[3]; /* Valores anteriores de la señal de control */
+  static double uk1[3]; /* Valores anteriores de la seï¿½al de control */
   
   /* Valores para el PID absoluto */
   static double Int_e[3],dt_e[3];
@@ -18,9 +18,7 @@
   /* Valores de los parametros del PID */
   double Ti[3],Td[3],Kp[3];
 
-  
-
-  
+   
   Ti[0]=0.04;  Td[0]=0.1;  Kp[0]=1617.76;
   Ti[1]=0.04;  Td[1]=0.1;  Kp[1]= 2537.76; 
   Ti[2]=0.04;  Td[2]=0.1;  Kp[2]=431.08; 
---
+++ modDinamico_Inverso_Real_SR.m
@@ -0,0 +1,39 @@
+function [udd]=modDinamico_Inverso_Real_SR(in)
+q1=in(1);
+q2=in(2);
+q3=in(3);
+
+qd1=in(4);
+qd2=in(5);
+qd3=in(6);
+
+qddr1=in(7);
+qddr2=in(8);
+qddr3=in(9);
+
+qddr=[qddr1 qddr2 qddr3]';
+qd=[qd1 qd2 qd3]';
+
+
+g=9.8;L0=0.6;L1=0.6;L2=1;L3=0.8;
+R1=1; R2=1; R3=1;    
+
+% Matriz de Inercias
+M=[ 4.434*cos(2.0*q2 + q3) + 5.7477*cos(2.0*q2) + 4.434*cos(q3) + 5.7389*cos(2.0*q2 + 2.0*q3) + 0.46597,                       0,                       0;
+                                                                                                   0,  11.085*cos(q3) + 21.67, 5.5425*cos(q3) + 6.3549;
+                                                                                                   0, 6.3343*cos(q3) + 7.2628,                    7.56];
+ 
+
+% Matriz de aceleraciones centr�petas y de Coriolis
+V=[ -1.3878e-18*qd1*(6.3901e18*qd2*sin(2.0*q2 + q3) + 3.195e18*qd3*sin(2.0*q2 + q3) + 8.2833e18*qd2*sin(2.0*q2) + 3.195e18*qd3*sin(q3) + 8.2706e18*qd2*sin(2.0*q2 + 2.0*q3) + 8.2706e18*qd3*sin(2.0*q2 + 2.0*q3) - 2.6192e16);
+                                                                0.15202*qd2 - 5.5425*qd3^2*sin(q3) + 7.1736*qd1^2*sin(2.0*q2 + 2.0*q3) + 5.5425*qd1^2*sin(2.0*q2 + q3) + 7.1847*qd1^2*sin(2.0*q2) - 11.085*qd2*qd3*sin(q3);
+                                                                                            0.018533*qd3 + 3.1671*qd1^2*sin(q3) + 6.3343*qd2^2*sin(q3) + 8.1984*qd1^2*sin(2.0*q2 + 2.0*q3) + 3.1671*qd1^2*sin(2.0*q2 + q3)];
+ 
+% Par gravitatorio                                                          
+G=[                                           0;
+ 2.5*g*(2.217*cos(q2 + q3) + 6.7509*cos(q2));
+                       6.3343*g*cos(q2 + q3)];
+
+udd=M.*qddr+V.*qd+G;
+                                                                                                                                                                                                udd=[M(1,1)*qddr1 M(2,2)*qddr2 M(3,3)*qddr3];                       
+return
