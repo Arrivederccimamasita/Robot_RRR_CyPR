8f55a98b895e2354dd4edb1868d7152a14ca569e
marrcogrova <marrcogrova@gmail.com>
2018-12-26 04:44:25 +0100

    Añadido el cuarto experimento, en el cual se evalúa el par calculado y la velocidad de los mismos

--- pruebasCONTROL.m
+++ pruebasCONTROL.m
@@ -4,8 +4,8 @@ clc % Clean command window
 Tm=0.001;
 
 % Definicion de las reductoras (Comentar una de las dos)
-% R1=1; R2=1; R3=1;
-R1=50; R2=30; R3=15;
+R1=1; R2=1; R3=1;
+%R1=50; R2=30; R3=15;
 
 % Definicion del tiempo de simulacion
 t_sim=5;
@@ -13,15 +13,21 @@ t_sim=5;
 % Deficicion de los valores necesarios para el GDT
 % EXPERIMENTO 1 Y 2
 % XYZ_init=[1.8 ; 0 ; 1.2];
-% XYZ_fin =[1.5071 ; 0 ; 0.4929];
+% XYZ_fin =[1.5071 ; 0 ; 0.4929];  % var articulares -> [0;-pi/4;pi/4]
 %t_mov=3;
 
 %EXPERIMENTO 3
-XYZ_init=[1.8 ; 0 ; 1.2];
-XYZ_fin=[0.9;0.9;1.0586];   % var articulares -> [pi/4;-pi/4;pi/2]
+%XYZ_init=[1.8 ; 0 ; 1.2];
+%XYZ_fin=[0.9;0.9;1.0586];   % var articulares -> [pi/4;-pi/4;pi/2]
+%t_mov=2;                    
+
+% EXPERIMENTO 4
+XYZ_init=[1.8;0;1.2];
+%XYZ_init =[1.266 ; 0 ; 0.00718];    % var articulares -> [0;-pi/6;-pi/6]
+XYZ_fin =[0.4;0.4;2.7657];      % var articulares -> [pi/4;pi/2;-pi/4]
 n_ptos=5;                   % Numero de ptos empleado para la trayectoria
 t_init_tray=2;              % Tiempo de inicio de la trayectoria
-t_mov=2;                    % Duracion del movimiento
+t_mov=1;                    % Duracion del movimiento
 
 % Definicion de las variables articulares iniciales del robot
 q1_init=0; q2_init=0; q3_init=0;
